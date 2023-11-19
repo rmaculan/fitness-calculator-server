627be2bb7f500d88420c603a9a6af409cf2a2780
Robert Maculan <robertmaculan@gmail.com>
2023-11-19 16:14:11 -0500

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("Hi!"));
+
+const PORT =  4000;
+
+app.listen(PORT, () => 
+console.log("Listening on port"));
